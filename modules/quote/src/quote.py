7ec0f6d49367083e50a27ac892fc1abe9427dc72
liriondev <liriondev@gmail.com>
2023-01-05 22:27:52 +0200

    final

--- modules/quote/src/quote.py
+++ modules/quote/src/quote.py
@@ -5,10 +5,14 @@ from PIL import Image
 
 @language
 async def quote(app,m,me,args,language):
-    avatar = await app.download_media([i async for i in app.get_chat_photos(m.reply_to_message.from_user.id)][0].file_id, in_memory=True)
-    url = "https://quotes.fl1yd.su/generate"
-    avatar = base64.b64encode(avatar.getvalue()).decode()
-    params = json.dumps({
+    if m.reply:
+        try:t=await m.edit("**Generation....**")
+        except:t=await m.reply("**Generation...**")
+        try:avatar = await app.download_media([i async for i in app.get_chat_photos(m.reply_to_message.from_user.id)][0].file_id, in_memory=True)
+        except:avatar=None
+        url = "https://quotes.fl1yd.su/generate"
+        avatar = base64.b64encode(avatar.getvalue()).decode()
+        params = json.dumps({
         "messages": [
           {
                "text": m.reply_to_message.text,
@@ -18,21 +22,23 @@ async def quote(app,m,me,args,language):
         ],
         "quote_color": "#162330",
         "text_color": "#fff",
-    }, ensure_ascii=False)
+        }, ensure_ascii=False)
 
-    response = requests.post(
-        url, 
-        data=params.encode("utf-8"),
-        headers={'Content-Type': 'application/json; charset=UTF-8'},
-    )
+        response = requests.post(
+            url, 
+            data=params.encode("utf-8"),
+            headers={'Content-Type': 'application/json; charset=UTF-8'},
+        )
 
-    tmp = BytesIO()
-    tmp.name = "test"
+        tmp = BytesIO()
+        tmp.name = "test"
 
-    quote = BytesIO(response.content)
-    quote.name = "test"
+        quote = BytesIO(response.content)
+        quote.name = "test"
 
-    image = Image.open(quote)
-    image.save(tmp, format="webp")
+        image = Image.open(quote)
+        image.save(tmp, format="webp")
 
-    await app.send_sticker(m.chat.id, tmp, reply_to_message_id=m.reply_to_message.id)
+        await t.delete()
+
+        await app.send_sticker(m.chat.id, tmp, reply_to_message_id=m.reply_to_message.id)
