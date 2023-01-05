e36547b28f7e42a4d919b7a43510cc36115d87e7
liriondev <liriondev@gmail.com>
2023-01-05 22:27:52 +0200

    And...

--- modules/quote/src/quote.py
+++ modules/quote/src/quote.py
@@ -4,13 +4,14 @@ from io import BytesIO
 
 @language
 async def quote(app,m,me,args,language):
-	avatar = [i async for i in app.get_chat_photos(m.reply_to_message.from_user.id)][0].file_id
-	url = "https://quotes.fl1yd.su/generate"
+	avatar = await app.download_media([i async for i in app.get_chat_photos(m.reply_to_message.from_user.id)][0].file_id, in_memory=True)
+    url = "https://quotes.fl1yd.su/generate"
+    avatar = base64.b64encode(avatar.readbuff()).decode()
 	params = {
         "messages": [
           {
                "text": m.reply_to_message.text,
-               "author": {"id": m.reply_to_message.from_user.id,"name": m.reply_to_message.from_user.first_name,"avatar": avatar},
+               "author": {"id": m.reply_to_message.from_user.id,"name": m.reply_to_message.from_user.first_name,"avatar":avatar},
                "reply": {}
            }
         ],
