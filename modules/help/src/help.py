806150d20580e08a5ce6ea9d422de250caf63ea1
liriondev <liriondev@gmail.com>
2023-01-05 22:27:52 +0200

    fix md

--- modules/help/src/help.py
+++ modules/help/src/help.py
@@ -6,11 +6,11 @@ async def help(app,m,me,args,language):
   try:
     module=loader.load(args[1]).info
     msg=f'''
-**{language.name}:** ```{module.module_name}```
-**{language.info}:** ```{module.module_info}```
+**{language.name}:** `{module.module_name}`
+**{language.info}:** `{module.module_info}`
 
 **{language.help}:**
-```{module.module_help}```
+`{module.module_help}`
             '''
     await m.edit(msg)
   except: await m.edit(language.error)
